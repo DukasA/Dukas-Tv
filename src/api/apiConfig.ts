07cda7bed70bbbd43a50ad0fc0606eefd3652515
Dukas Kanon <lolololokokokorfff@gmail.com>
2023-04-13 10:44:55 +0200

    rm api_key

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,6 @@
 export const apiConfig = {
   baseUrl: 'https://api.themoviedb.org/3/',
-  API_KEY: 'd78a4e3c6b8ed190b0d538f8965c8bbe',
+  API_KEY: '',
   originalImage: (imgPath: string) =>
     `https://api.themoviedb.org/t/p/original/${imgPath}`,
 };
