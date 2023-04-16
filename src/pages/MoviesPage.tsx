ff79c3b16825ac3a8f0b7d62e0c6af763d93f087
Dukas Kanon <lolololokokokorfff@gmail.com>
2023-04-16 16:00:38 +0200

    add types for axios in moviePage

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -7,11 +7,18 @@ import { load } from '../store/reducers/moviesReducer';
 
 export const MoviesPage: React.FC = () => {
   const dispatch = useDispatch();
-
+  interface IMovie {
+    movie: {
+      id: number;
+      title: string;
+      release_date: string;
+      backdrop_path: string;
+    };
+  }
   useEffect(() => {
     const getMovies = async () => {
       try {
-        const response = await axios.get(
+        const response = await axios.get<{ results: IMovie[] }>(
           `https://api.themoviedb.org/3/discover/movie?api_key=${apiConfig.API_KEY}&sort_by=popularity.desc&without_genres=16,10751&page=1`,
         );
         dispatch(load(response.data.results));
@@ -22,6 +29,7 @@ export const MoviesPage: React.FC = () => {
 
     getMovies();
   }, []);
+
   return (
     <div className="">
       <MoviesContainer />
